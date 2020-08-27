992e6e49c6ececf7e3abe66192b039736817be79
SRETeamofTEN <69629251+SRETeamofTEN@users.noreply.github.com>
2020-08-28 01:44:44 +0300

    Update DomainDB_pipeline.groovy

--- DomainDB_pipeline.groovy
+++ DomainDB_pipeline.groovy
@@ -1,6 +1,6 @@
 def checkStatuses = "".toString();
 def toAddress = "hristo.popov@sap.com".toString()
-def toAddressFailover = "matthias.gradl@sap.com,valentin.zipf@sap.com, DL_59BA2AAB5F99B79157000011@exchange.sap.corp".toString()
+def toAddressFailover = "hristo.popov@sap.com".toString()
 
 //Email has to be redone after adding thousand eyes and Dynatrace integrations in 2 more stages
 
